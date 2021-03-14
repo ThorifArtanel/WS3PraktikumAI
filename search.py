c200682a10e4705eceaa018242148ff2e6261ba7
ThorifArtanel <35924151+ThorifArtanel@users.noreply.github.com>
2021-03-14 13:45:58 +0700

    Update search.py
    openMap masih gak jalan :)

--- search.py
+++ search.py
@@ -17,18 +17,52 @@
 import queue
 
 def search(maze):
-	path = []
-	current = ()
-	current = maze.getStart()
-	row,col = current
-	path.append(current)
-	a = maze.getNeighbors(row,col)
-	path.append(a[0])
-	i = 1
-	while ( (i < 10) and (not maze.isObjective(row,col))):
-		current = maze.getNeighbors(row,col)
-		row,col = current[0]
-		path.append(current[0])
-		i = i + 1
+    fringe = queue.Queue()
+    fringe.put(maze.getStart())
+    explored = []
+    parent = {}
 
-	return path
+    while not fringe.empty():
+        current = fringe.get()
+        explored.append(current)
+        row = current[0]
+        col = current[1]
+        if maze.isObjective(row, col):
+            print("found" + "ROW: " + str(row) + " COL: " + str(col))
+            return compileEndResult(maze.getStart(), (row, col), parent)
+
+        print("row: " + str(row) + " col: " + str(col))
+        newFringe = maze.getNeighbors(row, col)
+        for x, y in enumerate(newFringe):
+            if not isExist(y, explored):
+                fringe.put(y)
+                parent[y] = current
+
+    for x, y in enumerate(explored):
+        row = y[0]
+        col = y[1]
+        print("row: " + str(row) + " col: " + str(col))
+
+    return []
+
+
+def isExist(newFringe, explored):
+    for x, y in enumerate(explored):
+        if y == newFringe:
+            return True
+
+    return False
+
+
+def compileEndResult(initialState, endState, parent):
+    endResult = []
+    endResult.insert(0, endState)
+
+    while initialState != endState:
+        for item in parent.items():
+            if item[0] == endState:
+                endResult.insert(0, item[1])
+                endState = item[1]
+                break
+
+    return endResult
